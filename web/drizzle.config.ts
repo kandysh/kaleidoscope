1bd925713dd10cd0b7ba7adcdaf235b61addb9b8
kandysh <rajawat.k.029@gmail.com>
2025-01-22 21:52:38 +0530

    env fix

--- web/drizzle.config.ts
+++ web/drizzle.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   out: "./lib/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.UP_POSTGRES_URL as string,
+    url: process.env.DATABASE_URL as string,
+
   },
   verbose: true,
   strict: true,
