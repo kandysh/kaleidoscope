b7dd8b4fcff8cb1788cdf6bbff89b0af03db0dd9
kandysh <rajawat.k.029@gmail.com>
2025-01-23 01:02:54 +0530

    added bull mq and redis

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { fetchVideoUrl, upload } from "@/actions/upload";
+import { fetchVideoUrl, pushToVideoQueue, upload } from "@/actions/upload";
 import React, { useState } from "react";
 
 export default function Home() {
@@ -28,6 +28,17 @@ export default function Home() {
     })
   }
 
+  async function handleClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    console.log(event);
+    await pushToVideoQueue({
+      id: mediaId!,
+      name: file!.name,
+      type: file!.type,
+      size: file!.size,
+      lastModiiedDate: new Date(file!.lastModified)
+    });
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <button type="button" onClick={async () => {
@@ -47,6 +58,11 @@ export default function Home() {
         }} />
         <button type="submit">Submit</button>
       </form>
+
+
+      <button type="button" onClick={handleClick}>
+        Click to send events
+      </button>
     </div>
   );
 }
